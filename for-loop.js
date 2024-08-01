2a76c0835f271dbe7acfa78cb50fe5f744e6c5ea
Shahrukhkhan-rabby <shahrukhkhan3418198@gmail.com>
2024-08-01 14:05:57 +0600

    array

---
+++ for-loop.js
@@ -0,0 +1,29 @@
+// var roastGiven = 0;
+// while(roastGiven < 7){
+//     console.log('Roast den, please!!')
+//     roastGiven++;
+// }
+
+
+
+
+
+
+// for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
+//     console.log('Roast den, please!!');
+//     console.log(roastGiven)
+// i += 2; i = i + 2 }
+
+
+// for(var i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+
+// for(var i = 1; i <= 20; i+= 2){
+//     console.log(i);
+// }
+
+// for(var i = 0; i <= 20; i +=2){
+//     console.log(i);
+// }
--- while-lool-examplr.js
+++ while-lool-examplr.js
@@ -1,5 +1,19 @@
-var number = 0 
-while (number <= 20){
+// var number = 1; 
+// while (number <= 20){
+//     console.log(number);
+//     number = number+2;
+// }
+
+// odd numbers
+// var number = 1;
+// while (number <= 20){
+//     console.log(number);
+// //     number = number +2;
+// }
+
+// even numbers
+number = 0;
+while(number <= 20){
     console.log(number);
-    number = number+2;
+    number = number +2;
 }
